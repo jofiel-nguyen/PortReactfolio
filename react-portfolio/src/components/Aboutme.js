cd897d5c440ac5b1f5d8a71efa302ba52975d050
Wren <thitunguyen@my.lonestar.edu>
2023-07-14 16:11:08 -0500

    update style

--- react-portfolio/src/components/Aboutme.js
+++ react-portfolio/src/components/Aboutme.js
@@ -5,9 +5,7 @@ function AboutMe() {
   return (
     <section id="about-me">
       <h2>About Me</h2>
-      <a href="/contact/aboutme.html">
         <img src={aboutMeImage} alt="" width="200" height="200" />
-      </a>
       <p>
         <em>Coding, gaming, and creating have always been my passions. There's something magical about the creative process that drives me to spend countless hours tinkering with code, exploring new game mechanics, and crafting unique works of art.</em>
       </p>
--- react-portfolio/src/components/Portfolio.js
+++ react-portfolio/src/components/Portfolio.js
@@ -3,24 +3,27 @@ import project1Image from '../assets/images/project1.png';
 import project2Image from '../assets/images/project2.png';
 
 function Portfolio() {
+  const imageStyle = {
+    width: '100%',
+    height: '200px', // Adjust the height to your desired size
+    objectFit: 'cover', // To maintain the aspect ratio and fill the container
+    borderRadius: '4px',
+  };
+
   return (
     <section id="my-work">
-      <h2>My Work</h2>
-      <ul>
-        <li>
+      <h2 style={{ textAlign: 'center' }}>My Work</h2>
+      <ul style={{ display: 'flex', justifyContent: 'space-between', listStyle: 'none', padding: 0 }}>
+        <li style={{ width: '45%', border: '1px solid #ccc', borderRadius: '4px', padding: '20px' }}>
           <h3>Ping Pong Game</h3>
-          <a href="/project/pinponggame.html" target="_blank" rel="noopener noreferrer">
-            <img src={project1Image} alt="" width="300" height="200" />
-          </a>
+          <img src={project1Image} alt="" style={imageStyle} />
           <p>
             <em>Ping pong game that takes place in a galaxy environment.</em>
           </p>
         </li>
-        <li>
+        <li style={{ width: '45%', border: '1px solid #ccc', borderRadius: '4px', padding: '20px' }}>
           <h3>My Weather App</h3>
-          <a href="/project/getweather.html" target="_blank" rel="noopener noreferrer">
-            <img src={project2Image} alt="" width="300" height="200" />
-          </a>
+          <img src={project2Image} alt="" style={imageStyle} />
           <p>
             <em>HTML, JavaScript, and the Fetch API were used to create a website to check the next 3 or 5 days of weather in a chosen location.</em>
           </p>
