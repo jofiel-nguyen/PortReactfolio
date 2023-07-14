c25e594b3ebc797d779b2ef2ff3b88d7d9f74b49
Wren <thitunguyen@my.lonestar.edu>
2023-07-13 21:01:00 -0500

    update fixed display image

--- react-portfolio/src/components/Portfolio.js
+++ react-portfolio/src/components/Portfolio.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import project1Image from '../assets/images/project1.png';
+import project2Image from '../assets/images/project2.png';
 
 function Portfolio() {
   return (
@@ -7,23 +9,22 @@ function Portfolio() {
       <ul>
         <li>
           <h3>Ping Pong Game</h3>
-          <a href="../project/pinponggame.html" target="_blank">
-            <img src="../assets/images/project1.png" alt="" width="300" height="200" />
+          <a href="/project/pinponggame.html" target="_blank" rel="noopener noreferrer">
+            <img src={project1Image} alt="" width="300" height="200" />
           </a>
           <p>
             <em>Ping pong game that takes place in a galaxy environment.</em>
           </p>
         </li>
         <li>
-          <h3>My Weather Apps</h3>
-          <a href="../project/getweather.html" target="_blank">
-            <img src="../assets/images/project2.png" alt="" width="300" height="200" />
+          <h3>My Weather App</h3>
+          <a href="/project/getweather.html" target="_blank" rel="noopener noreferrer">
+            <img src={project2Image} alt="" width="300" height="200" />
           </a>
           <p>
-            <em>Html, JavasScript and Fetch API was used for creating a website to check the next 3 or 5 days in the location users want.</em>
+            <em>HTML, JavaScript, and the Fetch API were used to create a website to check the next 3 or 5 days of weather in a chosen location.</em>
           </p>
         </li>
-      
       </ul>
     </section>
   );
