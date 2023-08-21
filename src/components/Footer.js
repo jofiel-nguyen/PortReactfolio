3a7a93389c847c8c62643b218595b6a0947ca42f
Nhu-Nguyen <121688890+nhunguyen-debug@users.noreply.github.com>
2023-08-21 11:00:08 -0500

    add twitter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,62 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin, faStackOverflow, faTiktok, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import '../App.css'
+import {
+  faGithub,
+  faLinkedin,
+  faStackOverflow,
+  faTiktok,
+  faTwitter,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
+import '../App.css';
+
 function Footer() {
   return (
     <footer>
       <div className="footer-content">
         <p>&copy; 2023 Wren's Portfolio</p>
         <div className="social-links">
-          <a href="https://github.com/nhunguyen-debug" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://github.com/nhunguyen-debug"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon icon={faGithub} className="icon" />
           </a>
-          <a href="https://www.linkedin.com/in/wren-nguyen-a63359269/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.linkedin.com/in/wren-nguyen-a63359269/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon icon={faLinkedin} className="icon" />
           </a>
-          <a href="https://stackoverflow.com/users/22229533/nhu-nguyen" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://stackoverflow.com/users/22229533/nhu-nguyen"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon icon={faStackOverflow} className="icon" />
           </a>
-          <a href="https://tiktok.com/@wrenyvette" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://tiktok.com/@wrenyvette"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon icon={faTiktok} className="icon" />
           </a>
-          <a href="https://www.youtube.com/channel/UCztwHDEn_gZY8GRpLKSBGZA" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.youtube.com/channel/UCztwHDEn_gZY8GRpLKSBGZA"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon icon={faYoutube} className="icon" />
+          </a>
+          <a
+            href="https://twitter.com/wyn0925"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faTwitter} className="icon" />
           </a>
         </div>
       </div>
